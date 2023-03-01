ba9e3b8869bc55587533050ca2535300e97f491d
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-03-01 21:37:18 +0530

    Check weather two Strings are Anagrams or not

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Sorting Techniques");
+            Console.WriteLine("Welcome to Sorting Techniques\n");
+
+            Console.WriteLine("Checking for Anagram of two Strings...\n");
+
+            Anagrams anagram = new Anagrams();
+
+            anagram.DisplayAnagram();
         }
     }
 }
+    
+
